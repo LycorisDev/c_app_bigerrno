d56cacf41081d80aaa05c71451960d834dea32ea
Lycoris Radiata <lycoris.radiata.mofumofu@gmail.com>
2025-01-24 17:50:48 +0100

    Add notes about signals

--- executor/file.c
+++ executor/file.c
@@ -4,6 +4,34 @@ static int	open_infile(t_pl *pl, size_t i, int catch_err);
 static int	open_outfile(t_pl *pl, size_t i, int catch_err);
 static int	check_file(t_pl *pl, char *file, int mode, int catch_err);
 
+/*
+	TODO
+	----------------------------------------------------------------------------
+
+	- Is `check_file` useful? Can't we just open the file and see what error 
+	comes up?
+
+	SIMPLER HANDLING
+	- Initialize fd_src[0] to STDIN_FILENO and fd_src[1] to STDOUT_FILENO, then 
+	to the pipes if any, and then tackle the redirections. For a given infile, 
+	close fd_src[0] if it wasn't STDIN_FILENO, open the infile, and update 
+	fd_src[0] with its fd. Do the same for outfiles.
+	- After the redirections, do dup2, and close fd_src[0] and fd_src[1].
+
+	SIGNALS
+	- Check the flavor text with a command such as cat. It's a handwritten 
+	program which sends a signal to itself. Indeed, there's an issue with 
+	SIGQUIT for example. It definitely prints something, and yet my research 
+	led me to believe it doesn't.
+	- Add flavor text for all signals.
+	- Consider handling all signals in the shell itself (external commands are 
+	already handled).
+
+	PARSING
+	- Add a label to tokens for a simpler parsing. You'd be able to remove the 
+	parentheses hack.
+*/
+
 int	set_fd_src_from_files(t_pl *pl, int catch_err)
 {
 	size_t	i;
